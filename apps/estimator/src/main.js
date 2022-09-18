c2c54ebeb471349e825ed9de8d158169e52ba9de
ratautas <68229568+algirdas-zyro@users.noreply.github.com>
2022-09-18 19:56:36 +0300

    Update packates

--- apps/estimator/src/main.js
+++ apps/estimator/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import * as Vue from "vue";
 import { enableVueBindings } from "@syncedstore/core";
+import ElementPlus from 'element-plus'
+
 enableVueBindings(Vue);
 
 import App from "./App.vue";
@@ -9,7 +11,7 @@ import router from "./router";
 
 const app = createApp(App);
 
-// app.use(ElementPlus);
+app.use(ElementPlus);
 app.use(createPinia());
 app.use(router);
 
